85b9a38ec9ba3c8054b4c3d220f19059a27300c9
ToxaZ <az@dreamindustries.co>
2016-02-22 22:16:58 +0300

    log regression refactoring

--- log_regression.py
+++ log_regression.py
@@ -15,38 +15,26 @@ from sklearn.metrics import roc_auc_score
 from utils import write_submission
 
 
-import pandas as pd
-import numpy as np
-import math
-from sys import maxint
-from sklearn.metrics import roc_auc_score
-from utils import write_submission
+# splitting weights updater to 2 different functions is easier to handle
+def update_weights(X, y, weights, k):
+    dp = np.einsum('ij,j->i', X, weights[-1])
+    c_matrix = 1-(1./(1+np.exp(-y*dp)))
+    a = k/c_matrix.shape[0]*(np.einsum('ij,i,i->j', X, y, c_matrix))
+    weights_list = []
+    for w in xrange(len(weights[0])):
+        weights_list.append(weights[-1][w] + a[w])
+    weights.append(weights_list)
+    return weights
 
 
-def create_gradient_descent(X, y, weights, k_step,
-                            e_distance, iterations_count=maxint,
-                            reg_coef=None, regularization=False):
-
-    def update_weights(X, y, weights, reg_coef, k_step, regularization):
-        dp = np.einsum('ij,j->i', X, weights[-1])
-        c_matrix = 1 - (1. / (1 + np.exp(-y * dp)))
-        a = k_step/c_matrix.shape[0]*(np.einsum('ij,i,i->j', X, y, c_matrix))
-        if regularization is False:
-            reg_coef = 0
-        weights_list = []
-        for w in xrange(len(weights[0])):
-            weights_list.append(weights[-1][w] + a[w] -
-                                k_step * reg_coef * weights[-1][w])
-        weights.append(weights_list)
-        return weights
-
-    e = 10000
-    for step in xrange(iterations_count):
-        while math.sqrt(e) > e_distance:
-            weights = update_weights(X, y, weights,
-                                     reg_coef, k_step, regularization)
-            for w in xrange(len(weights[0])):
-                e += (weights[-1][0] - weights[-2][0]) ** 2
+def update_weights_reg(X, y, weights, c, k):
+    dp = np.einsum('ij,j->i', X, weights[-1])
+    c_matrix = 1-(1./(1+np.exp(-y*dp)))
+    a = k/c_matrix.shape[0]*(np.einsum('ij,i,i->j', X, y, c_matrix))
+    weights_list = []
+    for w in xrange(len(weights[0])):
+        weights_list.append(weights[-1][w] + a[w] - k*c*weights[-1][w])
+    weights.append(weights_list)
     return weights
 
 
@@ -63,24 +51,42 @@ def main():
     data = np.genfromtxt('data/data-logistic.csv', delimiter=",")
     y = data[:, 0]
     X = data[:, 1:]
-    reg_coef = 10
-    reg_step = 0.1
-    start_weights = [[0.0, 0.0]]
-    euclidean_distance = 0.00001
-    iterations_max = 10000
+    e_convergence = 0.00001
+    max_iteration = 10000
+    c_reg = 10
+    k_step = 0.1
+    weights_start = [[0.0, 0.0]]
 
-    weights_matrix = create_gradient_descent(
-        X, y, start_weights, reg_step,
-        euclidean_distance, iterations_max
-    )
-    reg_weights_matrix = create_gradient_descent(
-        X, y, start_weights, reg_step,
-        euclidean_distance, iterations_max,
-        reg_coef, regularization=True
-    )
+    euclidean_distance = maxint
+    iteration_count = 0
+    while (math.sqrt(euclidean_distance) > e_convergence and
+            iteration_count < max_iteration):
+        weights_gradient = update_weights(X, y, weights_start, k_step)
 
-    y_scores = [sigmoid(X[i], weights_matrix[-1]) for i in xrange(len(X))]
-    y_scores_reg = [sigmoid(X[i], reg_weights_matrix[-1]) for i in xrange(len(X))]
+        euclidean_distance = 0
+        for w in xrange(len(weights_gradient[0])):
+            euclidean_distance += (weights_gradient[-1][w] -
+                                   weights_gradient[-2][w]) ** 2
+        iteration_count += 1
+    final_w = weights_gradient[-1]
+
+    euclidean_distance = maxint
+    iteration_count = 0
+    while (math.sqrt(euclidean_distance) > e_convergence and
+            iteration_count < max_iteration):
+        weights_gradient_reg = update_weights_reg(X, y, weights_start,
+                                                  c_reg, k_step)
+
+        euclidean_distance = 0
+        for w in xrange(len(weights_gradient_reg[0])):
+            euclidean_distance += (weights_gradient_reg[-1][w] -
+                                   weights_gradient_reg[-2][w]) ** 2
+        iteration_count += 1
+    final_w_reg = weights_gradient_reg[-1]
+
+    y_scores = [sigmoid(X[i].tolist(), final_w) for i in xrange(len(X))]
+    y_scores_reg = [sigmoid(X[i].tolist(), final_w_reg) for i in xrange(len(X))]
+
     write_submission(
         [round(roc_auc_score(y, y_scores), 3),
          round(roc_auc_score(y, y_scores_reg), 3)],
